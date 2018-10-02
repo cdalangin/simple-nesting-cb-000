6381aa1a2e763c4150c8caadce243addb80b4019
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-10-02 17:28:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -124,8 +124,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 denny = programmer_hash[:dennis_ritchie][:languages]
-		 denny = ["C", "Assembly"]
+		 denny = programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 		 programmer_hash
 
 end
